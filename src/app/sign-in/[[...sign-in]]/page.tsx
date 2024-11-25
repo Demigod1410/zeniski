079a33d534b993edf28c28eda4bf6ba9cab65b88
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-11-25 19:53:57 +0530

    added fallbackRedirectUrl && fixes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className="flex items-center justify-center">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="h-screen transform scale-[0.9] flex items-center">
-        <SignIn />
+        <SignIn fallbackRedirectUrl="/dashboard" />
       </div>
     </div>
   );
