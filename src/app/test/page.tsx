029d84ff002c1288265a933ef84522592e8f7976
Demigod1410 <demigod14100@gmail.com>
2024-12-12 15:01:02 +0530

    fixes

--- src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <h1> hello</h1>
-      </main>
-    );
-  }
-  
